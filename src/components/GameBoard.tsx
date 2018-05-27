bad261a6e27e8a5b1c54c6ab9b77bcee4c8a966c
SkalskiP <pskalski@student.agh.edu.pl>
2018-05-27 21:06:56 +0200

    SCSS preprocessor added + NavigationBar and GameBoard components made

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class GameBoard extends React.Component {
+  public render() {
+    return (
+    <div className={"GameBoard"}>
+        null
+    </div>
+    );
+  }
+}
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class NavigationBar extends React.Component {
+  public render() {
+    return (
+      <div className={"NavigationBar"}>
+        TIC TAC TOE ALFA
+      </div>
+    );
+  }
+}
